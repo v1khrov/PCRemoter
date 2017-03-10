6db72a618ab2f291923397a13e1ce77de3243669
Dmitry <dmvikhrov@hotmail.com>
2017-03-10 08:24:31 +0300

    First Commit

---
+++ PCRemoter.Web/PCRemoterService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace PCRemoter.Web
+{
+    /// <summary>
+    /// Summary description for PCRemoterService
+    /// </summary>
+    [WebService(Namespace = "http://itfi.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class PCRemoterService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
